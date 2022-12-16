bf0750304c325d6cb3c3514c67d7a0a98000f451
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-12-16 14:32:18 +0000

    wip: client/server integration

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { json } from 'body-parser';
 import apiRoutes from './routes/api-routes';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 
 // parse body of any incoming requests
 app.use(json());
@@ -14,4 +17,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction)=>{
     res.status(500).json({message: err.message});
 });
 
-app.listen(3000);
+app.listen(3001);
