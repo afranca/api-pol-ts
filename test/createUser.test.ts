997e2226ffefacbc138f1de3a123fa2079d58f73
skumarqa2010 <qa.kumar2010@gmail.com>
2022-12-21 10:23:30 +0000

    Increase test coverage

--- test/createUser.test.ts
+++ test/createUser.test.ts
@@ -11,19 +11,32 @@ describe("Create users", async () => {
 
 	it("Create admin type user ", async () => {
 		let response = await common.postRequest(userData.userWithRoleAndWithoutOccupation());
+		expect(response.body.entry.name).to.be.equal("Test User2");
+		expect(response.body.entry.age).to.be.equal(42);
+		expect(response.body.entry.role).to.be.equal("Head");
 		expect(response.body.entry.type).to.be.equal("admin");
-		await common.postRequest(userData.user1WithRoleAndWithoutOccupation());
+		response = await common.postRequest(userData.user1WithRoleAndWithoutOccupation());
+		expect(response.body.entry.name).to.be.equal("Test User6");
+		expect(response.body.entry.age).to.be.equal(46);
+		expect(response.body.entry.role).to.be.equal("Supervisor");
 		expect(response.body.entry.type).to.be.equal("admin");
 	});
 
 	it("Create employee type user ", async () => {
 		let response = await common.postRequest(userData.userWithoutRoleAndWithOccupation());
 		expect(response.body.entry.type).to.be.equal("employee");
+		expect(response.body.entry.name).to.be.equal("Test User3");
+		expect(response.body.entry.age).to.be.equal(40);
+		expect(response.body.entry.occupation).to.be.equal("Tester");
 	});
 
-	it("Create poweruser type user", async () => {
+	it.only("Create poweruser type user", async () => {
 		let response = await common.postRequest(userData.userWithRoleAndOccupation());
 		expect(response.body.entry.type).to.be.equal("poweruser");
+		expect(response.body.entry.name).to.be.equal("Test User1");
+		expect(response.body.entry.age).to.be.equal(45);
+		expect(response.body.entry.role).to.be.equal("Software Engineer");
+		expect(response.body.entry.occupation).to.be.equal("IT Professional");
 	});
 
 	it("Create user without role and occupation ", async () => {
--- test/testData/userData.ts
+++ test/testData/userData.ts
@@ -1,10 +1,4 @@
 class UserData {
-	userWithRoleAndOccupation() {
-		const apiBody = '{ "name": "Test User1", "age": 45, "occupation": "IT Professional", "role": "Software Engineer" }';
-
-		return apiBody;
-	}
-
 	userWithRoleAndWithoutOccupation() {
 		const apiBody = '{ "name": "Test User2", "age": 42,  "role": "Head" }';
 
@@ -12,13 +6,30 @@ class UserData {
 	}
 
 	user1WithRoleAndWithoutOccupation() {
-		const apiBody = '{ "name": "Test User6", "age": 41,  "role": "Head" }';
+		const apiBody = '{ "name": "Test User6", "age": 46,  "role": "Supervisor" }';
+
+		return apiBody;
+	}
+	userWithRoleAndOccupation() {
+		const apiBody = '{ "name": "Test User1", "age": 45, "occupation": "IT Professional", "role": "Software Engineer" }';
 
 		return apiBody;
 	}
 
 	userWithoutRoleAndWithOccupation() {
 		const apiBody = '{ "name": "Test User3", "age": 40, "occupation": "Tester" }';
+
+		return apiBody;
+	}
+
+	user1WithoutRoleAndWithOccupation() {
+		const apiBody = '{ "name": "Test User8", "age": 40, "occupation": "Game designer" }';
+
+		return apiBody;
+	}
+
+	user2WithoutRoleAndWithOccupation() {
+		const apiBody = '{ "name": "Test User9", "age": 40, "occupation": "Economist" }';
 
 		return apiBody;
 	}
@@ -31,6 +42,18 @@ class UserData {
 
 	user1WithRoleAndOccupation() {
 		const apiBody = '{ "name": "Test User5", "age": 38, "occupation": "Accountant", "role": "Account manager" }';
+
+		return apiBody;
+	}
+
+	user2WithRoleAndOccupation() {
+		const apiBody = '{ "name": "Test User6", "age": 50, "occupation": "Doctor", "role": "Dentist" }';
+
+		return apiBody;
+	}
+
+	user3WithRoleAndOccupation() {
+		const apiBody = '{ "name": "Test User7", "age": 52, "occupation": "Doctor", "role": "Surgeon" }';
 
 		return apiBody;
 	}
